83ffe32f326a360db2d84ffe42436dd596466c94
moiz9265 <moizarshadkk@gmail.com>
2020-06-10 19:22:34 +0500

    First Commit

---
+++ app.js
@@ -0,0 +1,5 @@
+alert('Welcome to My Website');
+alert('Error! Please Enter a valid Password');
+alert('Welcom to JS Land..\n Happy Coding');
+alert('Welcome to JS Land');
+alert('Happy Coding! \n '+<input type="checkbox" />+'Prevent this message from creating additional dialogs');
